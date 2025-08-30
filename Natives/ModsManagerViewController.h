f67598670be378af13eb87b0d7aa1fed4784c1fa
DrweiTest1 <364365203@qq.com>
2025-08-30 11:24:09 +0800

    Update ModsManagerViewController.h

--- Natives/ModsManagerViewController.h
+++ Natives/ModsManagerViewController.h
@@ -1,17 +1,8 @@
-//
-//  ModsManagerViewController.h
-//  AmethystMods
-//
-//  Created by Copilot on 2025-08-22.
-//
-
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ModsManagerViewController : UIViewController
-
-@property (nonatomic, copy, nullable) NSString *profileName;
 
 @end
 
